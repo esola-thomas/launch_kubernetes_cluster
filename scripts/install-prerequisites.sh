1869951f35cf0c05690e3659b94a357f5e2bb85e
Ernesto Sola-Thomas <ernesto@solathomas.com>
2025-03-13 23:32:48 -0400

    Update Docker GPG key download URL in prerequisites script

--- scripts/install-prerequisites.sh
+++ scripts/install-prerequisites.sh
@@ -23,7 +23,7 @@ if [[ "${OS}" == "ubuntu" || "${OS}" == "debian" ]]; then
     sudo apt-get update
     sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL {{% param "download-url-base" %}}/gpg -o /etc/apt/keyrings/docker.asc
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
     # Add the repository to Apt sources:
     echo \
